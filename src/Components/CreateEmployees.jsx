3d20cdfbda05a2bc2f5dd51562ae81be90950936
noa bouskila <noabouskil@gmail.com>
2024-08-12 15:19:59 +0200

    css 1ere partie

--- src/Components/CreateEmployees.jsx
+++ src/Components/CreateEmployees.jsx
@@ -3,6 +3,8 @@ import { EmployeesContext } from '../context/EmployeesContext';
 import states from '../state';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import '../App.css';
+
 
 function CreateEmployees() {
 
@@ -68,93 +70,97 @@ function CreateEmployees() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <div className='container'>
+            <form onSubmit={handleSubmit}>
 
-            <h1>Create Employees</h1>
+                <h1>Create Employees</h1>
 
-            <div>
-                <label htmlFor="firstName">First Name</label>
-                <input type="text" id="firstName" name="firstName" onChange={handleChange}  required/>
-            </div>
-            <div>
-                <label htmlFor="lastName">Last Name</label>
-                <input type="text" id="lastName" name="lastName" onChange={handleChange} required />
-            </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName" name="firstName" onChange={handleChange}  required/>
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName" name="lastName" onChange={handleChange} required />
+                </div>
 
-            {/* Integration pluggin REACT DATEPICKER */}
-            <div>
-                <label htmlFor="dateOfBirth">Date Of Birth</label>
-                {/* <input type="text" id="dateOfBirth" name="dateOfBirth" onChange={handleChange} /> */}
+                {/* Integration pluggin REACT DATEPICKER */}
+                <div>
+                    <label htmlFor="dateOfBirth">Date Of Birth</label>
+                    {/* <input type="text" id="dateOfBirth" name="dateOfBirth" onChange={handleChange} /> */}
 
-                <DatePicker 
-                    // selected => selectionne la date depuis l'etat employee
-                    selected={employee.dateOfBirth}
-                    // onChange => selectionne la nouvelle date dans linput
-                    // date : la nouvelle date selectionnée , name : le nom du champs est dateOfBirth
-                    onChange={(date) => handleDateChange(date, 'dateOfBirth')}
-                    dateFormat="MM/dd/yyyy"
-                    placeholderText="Select date of birth"
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="startDate">Start Date</label>
-                {/* <input type="text" id="startDate" name="startDate" onChange={handleChange} /> */}
+                    <DatePicker 
+                        // selected => selectionne la date depuis l'etat employee
+                        selected={employee.dateOfBirth}
+                        // onChange => selectionne la nouvelle date dans linput
+                        // date : la nouvelle date selectionnée , name : le nom du champs est dateOfBirth
+                        onChange={(date) => handleDateChange(date, 'dateOfBirth')}
+                        dateFormat="MM/dd/yyyy"
+                        placeholderText="Select date of birth"
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="startDate">Start Date</label>
+                    {/* <input type="text" id="startDate" name="startDate" onChange={handleChange} /> */}
 
-                <DatePicker
-                    selected={employee.startDate}
-                    onChange={(date) => handleDateChange(date , 'startDate')}
-                    dateFormat="MM/dd/yyyy"
-                    placeholderText="Select start date"
-                    required
-                />
-            </ div>
+                    <DatePicker
+                        selected={employee.startDate}
+                        onChange={(date) => handleDateChange(date , 'startDate')}
+                        dateFormat="MM/dd/yyyy"
+                        placeholderText="Select start date"
+                        required
+                    />
+                </ div>
 
 
-            <div>
-                <fieldset>
-                    <legend>Address</legend>
-                    <div>
-                        <label htmlFor="street">Street</label>
-                        <input id="street" name="street" type="text" onChange={handleAddressChange} required />
-                    </div>
-                    <div>
-                            <label htmlFor="city">City</label>
-                            <input id="city" name="city" type="text" onChange={handleAddressChange} required/>
-                    </div>
+                <div>
+                    <fieldset>
+                        <legend>Address</legend>
+                        <div>
+                            <label htmlFor="street">Street</label>
+                            <input id="street" name="street" type="text" onChange={handleAddressChange} required />
+                        </div>
+                        <div>
+                                <label htmlFor="city">City</label>
+                                <input id="city" name="city" type="text" onChange={handleAddressChange} required/>
+                        </div>
 
-                    <div>
-                        <label htmlFor="state">State</label>
-                        <select name="state" id="state"  onChange={handleAddressChange} required>
-                            {states.map((state)=>(
-                                <option key={state.abbreviation} value={state.name}>
-                                {state.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="zipCode">Zip Code</label>
-                        <input id="zipCode" name="zipCode" type="number" onChange={handleAddressChange} required />
-                    </div>
-                </fieldset>
-            </div>
+                        <div>
+                            <label htmlFor="state">State</label>
+                            <select name="state" id="state"  onChange={handleAddressChange} required>
+                                {states.map((state)=>(
+                                    <option key={state.abbreviation} value={state.name}>
+                                    {state.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div>
+                            <label htmlFor="zipCode">Zip Code</label>
+                            <input id="zipCode" name="zipCode" type="number" onChange={handleAddressChange} required />
+                        </div>
+                    </fieldset>
+                </div>
 
-            <div>
-                <label htmlFor="department">Department</label>
-                <select  id="department" name="department" onChange={handleChange} required>
-                    <option value="Sales">Sales</option>
-                    <option value="Marketing">Marketing</option>
-                    <option value="Engineering">Engineering</option>
-                    <option value="HumanResources">Human Resources</option>
-                    <option value="Legal">Legal</option>
-                </select>
-            </div>
-            
-            <button type='submit'>Save</button>
+                <div>
+                    <label htmlFor="department">Department</label>
+                    <select  id="department" name="department" onChange={handleChange} required>
+                        <option value="Sales">Sales</option>
+                        <option value="Marketing">Marketing</option>
+                        <option value="Engineering">Engineering</option>
+                        <option value="HumanResources">Human Resources</option>
+                        <option value="Legal">Legal</option>
+                    </select>
+                </div>
 
-            <p>{confirmationMessage}</p>
-        </form>
+                <button type='submit'>Save</button>
+
+                <p>{confirmationMessage}</p>
+            </form>
+
+        </div>
+
     );
 }
 
--- src/Components/EmployeesList.jsx
+++ src/Components/EmployeesList.jsx
@@ -1,47 +1,54 @@
 import React ,{useContext} from 'react';
 import { EmployeesContext } from '../context/EmployeesContext';
+import '../App.css';
+
 
 function EmployeesList() {
 
     const { employees } = useContext(EmployeesContext);
 
     return (
-        <div>
-          <h1>Employees List</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Date of Birth</th>
-                <th>Start Date</th>
-                <th>Street</th>
-                <th>City</th>
-                <th>State</th>
-                <th>Zip Code</th>
-                <th>Department</th>
-              </tr>
-            </thead>
-            <tbody>
-              {employees.map((employee, index) => (
-                <tr key={index}>
-                  <td>{employee.firstName}</td>
-                  <td>{employee.lastName}</td>
-                  {/* la valeur est bien interprétée comme un objet date et aussi pour la formater */}
-                  <td>{employee.dateOfBirth ? new Date(employee.dateOfBirth).toLocaleDateString() : 'Not set'}</td>
-                  <td> {employee.startDate ? new Date(employee.startDate).toLocaleDateString() : 'Not set'}</td>
+        <div className='container'>
 
-                  <td>{employee.address.street}</td>
-                  <td>{employee.address.city}</td>
-                  <td>{employee.address.state}</td>
-                  <td>{employee.address.zipCode}</td>
-                  <td>{employee.department}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            <div>
+                <h1>Employees List</h1>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Date of Birth</th>
+                        <th>Start Date</th>
+                        <th>Street</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zip Code</th>
+                        <th>Department</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {employees.map((employee, index) => (
+                        <tr key={index}>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        {/* la valeur est bien interprétée comme un objet date et aussi pour la formater */}
+                        <td>{employee.dateOfBirth ? new Date(employee.dateOfBirth).toLocaleDateString() : 'Not set'}</td>
+                        <td> {employee.startDate ? new Date(employee.startDate).toLocaleDateString() : 'Not set'}</td>
+
+                        <td>{employee.address.street}</td>
+                        <td>{employee.address.city}</td>
+                        <td>{employee.address.state}</td>
+                        <td>{employee.address.zipCode}</td>
+                        <td>{employee.department}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
         </div>
-      );
+        
+    );
 }
 
 export default EmployeesList;
